728a59ebe5c2ed778f18d5719f975db4ca29f4d3
fegonbar <138933154+fegonbar@users.noreply.github.com>
2023-07-07 17:28:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Teste / Teste')
+streamlit.text('Teste2 / Teste2')
+streamlit.text('Teste3 / Teste3')
