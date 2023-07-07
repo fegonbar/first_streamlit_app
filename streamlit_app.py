82e4a5b5edfae76409605b13c502909855dcf665
fegonbar <138933154+fegonbar@users.noreply.github.com>
2023-07-07 17:29:59 -0300

    Update streamlit_app.py
    novas alteracoes com 3 itens

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Teste / Teste')
-streamlit.text('Teste2 / Teste2')
-streamlit.text('Teste3 / Teste3')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
