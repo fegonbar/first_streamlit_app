391afbea5affb57e19219997e37d971a9f676921
fegonbar <138933154+fegonbar@users.noreply.github.com>
2023-07-17 15:18:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it the screen as table
+streamlit.dataframe(fruityvice_normalized)
+
